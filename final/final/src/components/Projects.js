f639e3ad1b6f0980b57cb65e5ea1f314fdc4d142
apala7778 <thetrialbyexistence@gmail.com>
2023-04-03 21:29:03 +0530

    added project cards

--- final/final/src/components/Projects.js
+++ final/final/src/components/Projects.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import Card from './Cards'
+import './Projects.css';
 
 const Projects = () => {
   return (
     <div className='project'>
+      <div className="hss"> Projects </div>
+   
+      <div className='content' >
       <Card />
+      <Card /> 
+      <Card />
+      
+      
+      </div>
+      <div className='content' >
+      <Card />
+      <Card /> 
+      <Card /> 
+      
+      
+      </div>
+      
     </div>
   )
 }
