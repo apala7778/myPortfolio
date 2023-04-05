24e677c3742c642b83205f836717c31866b6485d
apala7778 <thetrialbyexistence@gmail.com>
2023-04-06 00:42:07 +0530

    made changes to css

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import  './App.css';
 import Project from './components/Projects';
 import Tech from './components/Tech';
@@ -10,16 +11,19 @@ import {HashLink as Link} from 'react-router-hash-link';
 
 
 
+
+
 function App() {
+  
   return (
    <BrowserRouter>
     <div className="header">
       <div>
       <div className="menu">
         <div className="portfolio">Portfolio.</div>
-        <div className="nav">
-          <div className="nav-projects"><Link to="#project">Projects</Link></div>
-          <div className="nav-tech"> <Link to='#tech' >Technologies</Link>  </div>
+        <div  className="nav">
+          <div className="nav-projects"><Link to="#project" smooth={true} >Projects</Link></div>
+          <div className="nav-tech"> <Link to='#tech' smooth={true} >Technologies</Link>  </div>
           <div className="nav-social"> <a href="https://apalawrites.hashnode.dev/" target='_blank'  className='blog'>Blogs</a></div>
            </div>
         <div className="icons">
@@ -29,6 +33,7 @@ function App() {
         <a href="https://www.linkedin.com/in/apala-g-33765b226" target='_blank' ><BsLinkedin size='1.7rem' color='white' /></a>
         
         </div>
+        
       </div>
      <div className='hero'>
       <div className='mid'>
@@ -38,11 +43,11 @@ function App() {
         <div className='about'>
           Information Technology undergrade, seeking to apply
           competent development skills with collaboration, communication and passion.
-          I look forward to build my skills and contributing to the development of open-source projects,
+          I look forward to build my skills and contributing to the development of Open-Source projects,
           I also write technical blogs. Reach me for any Freelance opportunity.
         </div>
         <div >
-          <form className='mygithub'><button formAction='https://github.com/apala7778'> my github</button></form>
+          <form className='mygithub'><button formAction='https://github.com/apala7778' formTarget='_blank'> my GitHub</button></form>
           
         </div>
       </div> 
